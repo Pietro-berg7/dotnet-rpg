a5ffe57b4a082d7a338a94af1b3d396a35ba5140
Pietro <pietrovb25@gmail.com>
2024-01-13 10:15:43 -0300

    feat: add method put to modify a character

--- dotnet-rpg/Controllers/CharacterController.cs
+++ dotnet-rpg/Controllers/CharacterController.cs
@@ -30,4 +30,16 @@ public class CharacterController: ControllerBase
     {
         return Ok(await _characterService.AddCharacter(newCharacter));
     }
+
+    [HttpPut]
+    public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> UpdateCharacter(UpdateCharacterDto updatedCharacter)
+    {
+        var response = await _characterService.UpdateCharacter(updatedCharacter);
+        if (response.Data is null)
+        {
+            return NotFound(response);
+        }
+
+        return Ok(response);
+    }
 }
