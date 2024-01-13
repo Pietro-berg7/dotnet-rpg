12c78e3a416e3a09a025a673124e5c5ab446ae5a
Pietro <pietrovb25@gmail.com>
2024-01-12 23:35:43 -0300

    feat: add auto mapper

--- dotnet-rpg/Services/CharacterService/CharacterService.cs
+++ dotnet-rpg/Services/CharacterService/CharacterService.cs
@@ -1,4 +1,4 @@
-﻿using dotnet_rpg.Models;
+﻿global using AutoMapper;
 
 namespace dotnet_rpg.Services.CharacterService;
 
@@ -10,18 +10,27 @@ public class CharacterService: ICharacterService
         new() { Id = 1, Name = "Sam" }
     ];
 
+    private readonly IMapper _mapper;
+
+    public CharacterService(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
     public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
     {
         var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-        characters.Add(newCharacter);
-        serviceResponse.Data = characters;
+        var character = _mapper.Map<Character>(newCharacter);
+        character.Id = characters.Max(c => c.Id) + 1;
+        characters.Add(character);
+        serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
         return serviceResponse;
     }
 
     public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
     {
         var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-        serviceResponse.Data = characters;
+        serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
         return serviceResponse;
     }
 
@@ -29,7 +38,7 @@ public class CharacterService: ICharacterService
     {
         var serviceResponse = new ServiceResponse<GetCharacterDto>();
         var character = characters.FirstOrDefault(c => c.Id == id);
-        serviceResponse.Data = character;
+        serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
         return serviceResponse;
     }
 }
