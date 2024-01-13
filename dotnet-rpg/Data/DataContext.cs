795fda3dce6a07558411050e76427bf0075d478a
Pietro <pietrovb25@gmail.com>
2024-01-13 13:47:45 -0300

    feat: add dbcontext and connection string

---
+++ dotnet-rpg/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_rpg.Data;
+
+public class DataContext: DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options): base(options)
+    {
+    }
+
+    public DbSet<Character> Characters => Set<Character>();
+}
