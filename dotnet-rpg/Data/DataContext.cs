5082420c51626f2f2dcc56f0ba57d69329ccf3df
Pietro <pietrovb25@gmail.com>
2024-01-22 13:47:27 -0300

    feat: many-to-many relationship with skills

--- dotnet-rpg/Data/DataContext.cs
+++ dotnet-rpg/Data/DataContext.cs
@@ -9,4 +9,5 @@ public class DataContext: DbContext
     public DbSet<Character> Characters => Set<Character>();
     public DbSet<User> Users => Set<User>();
     public DbSet<Weapon> Weapons => Set<Weapon>();
+    public DbSet<Skill> Skills => Set<Skill>();
 }
